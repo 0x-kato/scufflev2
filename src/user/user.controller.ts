f79c3e746f5d901ad2ac598ee62d98f63ae5142a
0x-kato <ktocrake@gmail.com>
2024-02-05 18:46:14 -0500

    set up custom @GetUser decorator and created "getMe" fn

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { GetUser } from 'src/auth/decorator';
+import { JwtGuard } from 'src/auth/guard';
 
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
   //TODO: change from 'me' to username?
   //implement guard here so that users with bearer token get returned their entire user info
   //maybe add tip history to this route if pass guard, if not then return blank profile page?
   @Get('me')
-  getMe() {
-    return 'user info';
+  getMe(@GetUser() user: User) {
+    console.log({
+      user,
+    });
+    return user;
   }
 }
