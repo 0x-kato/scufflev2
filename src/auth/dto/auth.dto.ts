a5432b0e30d1aa0048fd25e5fd5405466e72d74e
0x-kato <ktocrake@gmail.com>
2024-02-07 18:09:21 -0500

    added validation pipes to AuthDto class

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,5 +1,14 @@
-export interface AuthDto {
+import { IsEmail, IsString, Length } from 'class-validator';
+
+export class AuthDto {
+  @IsString()
+  @Length(3, 20)
   username: string;
+
+  @IsEmail()
   email: string;
+
+  @IsString()
+  @Length(5, 100)
   password: string;
 }
